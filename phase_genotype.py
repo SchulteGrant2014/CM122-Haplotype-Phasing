9b0fb984c4a00cc7704260a768bc8e2800b8f04e
Grant Schulte <gschulte@ucla.edu>
2018-05-31 18:11:20 -0700

    Added function to compute the complementary haplotype to a given haplotype

--- phase_genotype.py
+++ phase_genotype.py
@@ -6,7 +6,17 @@ Created on Thu May 31 14:59:00 2018
 @author: grant
 """
 
+import time
 
+
+
+# This function is the recursive helper function for phaseGenotype().
+# It recursively computes all compatible haplotypes to the given genotype
+# for all positions up to the current SNP, then phases the current SNP and
+# updates the list of possible haplotypes to reflect including this new SNP.
+# @param genotype The genotype to be phased
+# @param i The current position in the genotype
+# @return All compatible haplotype phases of genotype up to position i
 def phaseGenotypeRecurse(genotype, i):
     
     # Base case: when we reach the beginning of the sequence
@@ -53,6 +63,11 @@ def phaseGenotypeRecurse(genotype, i):
     
 
 
+
+# This function takes a genotype and outputs all compatible haplotype phases
+# of that genotype.
+# @param genotype The genotype to be phased
+# @return All compatible haplotype phases of genotype
 def phaseGenotype(genotype):
     # Get phases up to the last SNP
     lastIndex = len(genotype) - 1
@@ -61,11 +76,69 @@ def phaseGenotype(genotype):
             
 
 
-"""
+
+# This function takes a haplotype and a genotype and outputs the complementary
+# haplotype to the one provided.
+# @param haplotype One of the haplotypes making up the genotype
+# @param genotype The genotype made up of the given haplotype and some
+# other unknown haplotype
+def haplotypeComplement(haplotype, genotype):
+    # Check that the haplotype and genotype provided are of the same length
+    if len(haplotype) != len(genotype):
+        raise ValueError("The lengths of Haplotype and Genotype arguments provided to haplotypeComplement() are not equal!")
+    
+    # Compare each haplotype position to the corresponding position in the genotype to derive the complementary haplotype
+    complement = []
+    for i in range(0, len(haplotype)):
+        # Check that each position is compatible... if not, raise an error
+        genotypeVal = genotype[i]
+        haplotypeVal = haplotype[i]
+        if ((genotypeVal == 2 and haplotypeVal != 1) or (genotypeVal == 0 and haplotypeVal != 0)):
+            raise ValueError("Haplotype is not compatible with genotype at index " + str(i) + " in function haplotypeComplement()!")
+        else:
+            complement.append(genotypeVal - haplotypeVal)
+    
+    # Return the complementary haplotype
+    return complement
+
+
+
+
+# Main function to run upon running this script
 if __name__ == "__main__":
-    genotype = [2, 0, 1, 1, 2, 2, 1]
-    print phaseGenotype(genotype)
-"""
+    
+    """
+    # Run the genotype phasing algorithm
+    start = time.time()
+    
+    #genotype = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    genotype = [2, 1, 0, 1, 0, 1, 2, 2, 0]
+    phases = phaseGenotype(genotype)
+    
+    end = time.time()
+    
+    numOnes = 0
+    for i in genotype:
+        if i == 1:
+            numOnes += 1
+    
+    print phases
+    print "Total number of haplotypes:", len(phases)
+    print "Length of genotype:", len(genotype)
+    print "Number of heterozygous positions:", numOnes
+    print "Phasing this genotype took " + str(end-start) + " seconds."
+    """
+    
+    # Run the haplotype complement algorithm
+    genotype = [2, 1, 0, 1, 0, 1, 2, 2, 0]
+    haplotype = [1, 0, 0, 1, 0, 0, 1, 1, 0]
+    complementaryHaplotype = haplotypeComplement(haplotype, genotype)
+    
+    print genotype
+    print haplotype
+    print complementaryHaplotype
+    
+
 
 
 
